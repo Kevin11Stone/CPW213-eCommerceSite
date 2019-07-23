a290dd5dacad36526e684cc4159269b65dab8781
ruca6616 <ruca6616@gmail.com>
2019-07-23 10:49:39 -0700

    Registered DBContext

--- eCommerce/Startup.cs
+++ eCommerce/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eCommerce.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -32,8 +34,31 @@ namespace eCommerce
             });
 
 
+            string connection = Configuration.GetConnectionString("GameDbConnection");
+
+            // https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-2.0#register-the-schoolcontext
+            // register DB Context
+            services.AddDbContext<GameContext>
+            (
+                options => options.UseSqlServer(connection)
+            );
+
+
+
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
+
+        // same as register DB Context lambda expression
+        private void ConfigureMyDbContext(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("con string");
+        }
+
+
+
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
