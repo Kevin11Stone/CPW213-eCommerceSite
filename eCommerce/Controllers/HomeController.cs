6a21dd618c1d9c4e8ca8a0f16299cfa52732d7bd
ruca6616 <ruca6616@gmail.com>
2019-07-25 09:44:54 -0700

    Added Library list page and game add page

--- eCommerce/Controllers/HomeController.cs
+++ eCommerce/Controllers/HomeController.cs
@@ -25,5 +25,9 @@ namespace eCommerce.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+
+
     }
 }
---
+++ eCommerce/Controllers/LibraryController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // get request for library/add, simply returns view
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(VideoGame game)
+        {
+            // model binding, if all data annotations validate
+            if (ModelState.IsValid)
+            {
+                // add to database
+                return RedirectToAction("Index");
+
+            }
+
+            // if not valid: return view with model including error messages
+            return View(game);
+
+        }
+
+
+    }
+}
