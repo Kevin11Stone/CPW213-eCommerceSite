b190ab94007ac32cd30ea884470beb29f2c1fea9
Kevin Stone <ruca6616@gmail.com>
2019-08-08 15:59:53 -0700

    Began register implementation. (Added AccountController, Register View)

---
+++ eCommerce/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        // add constructor with DB Context as a parameter
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Register(Member m)
+        {
+            // TODO: Add member to database
+            // Display success message on home page after redirecting
+
+
+        }
+
+
+    }
+}
